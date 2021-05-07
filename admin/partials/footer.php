ef0d88f5c697a7e6f5d0e081bce06c9f4a6cb332
Poushali Deb Purkayastha <65014940+Poushali0202@users.noreply.github.com>
2021-05-07 14:09:55 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Restaurant</p>
+            </div>
+        </div>
+    </body>
+</html>
