4aa62f5050df61bc35a4d6de78b5b290474fd925
Poushali Deb Purkayastha <65014940+Poushali0202@users.noreply.github.com>
2021-06-08 22:43:32 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -10,7 +10,7 @@
     define('DB_PASSWORD', '');
     define('DB_NAME', 'food-');
     
-    $conn = mysqli(mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD)); //or die(mysqli_error()); //Database Connection
+    $conn =( mysqli)mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD); //or die(mysqli_error()); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME); //or die(mysqli_error()); //SElecting Database
 
 
