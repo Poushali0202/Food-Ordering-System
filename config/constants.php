4ddcc47a724243a6ce7a48e24f14ee8d51d7ac7b
Poushali Deb Purkayastha <65014940+Poushali0202@users.noreply.github.com>
2021-06-08 22:26:15 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,15 +3,15 @@
     session_start();
 
 
-    //Create Constants to Store Non Repeating Values
-    define('SITEURL', 'http://localhost/food-order/');
+    //Create Constants to  Non Repeating Values
+    define('SITEURL', 'http://localhost/food-/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('DB_NAME', 'food-');
     
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
-    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
+    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) //or die(mysqli_error()); //Database Connection
+    $db_select = mysqli_select_db($conn, DB_NAME) //or die(mysqli_error()); //SElecting Database
 
 
 ?>
